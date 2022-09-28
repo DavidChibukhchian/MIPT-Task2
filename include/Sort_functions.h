fe67536c8e8e0243f2c2375a25e8d54196b6d8ca
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-09-29 01:59:05 +0300

    Update Sort_functions.h

--- include/Sort_functions.h
+++ include/Sort_functions.h
@@ -5,6 +5,6 @@ void swap(void* ptr1, void* ptr2, size_t size);
 
 void my_bubble_sort(void* array, size_t number_of_elements, size_t size, int(*compare)(const void*, const void*));
 
-void my_qsort(void* array, size_t number_of_elements, size_t size, int(*compare)(const void*, const void*));
+void my_qsort      (void* array, size_t number_of_elements, size_t size, int(*compare)(const void*, const void*));
 
 #endif //TASK2_SORT_FUNCTIONS_H
