32f62aae4ddbe4f5f1f31471a6b04cf3e3dc0697
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-09-29 01:57:25 +0300

    Update comparators.cpp

--- source/comparators.cpp
+++ source/comparators.cpp
@@ -2,18 +2,16 @@
 #include <assert.h>
 #include "Buffer.h"
 
+#define UNEXPECTED_MODE INT_MAX
+
 enum
 {
-    STRAIGHT_BGN =  1,
-    REVERSE_BGN  =  2,
-    STRAIGHT_END = -1,
-    REVERSE_END  = -2,
-
-    NEED_TO_CHECK_END_OF_STR = -3,
-
-    UNEXPECTED_MODE = -4,
+    STRAIGHT_BGN,
+    REVERSE_BGN,
+    STRAIGHT_END,
+    REVERSE_END,
+    NEED_TO_CHECK_END_OF_STR,
 };
-
 
 int bgn_compare_main_part(const char* str1, const char* str2, short mode)
 {
@@ -37,7 +35,6 @@ int bgn_compare_main_part(const char* str1, const char* str2, short mode)
             str2++;
             continue;
         }
-
         switch(mode)
         {
             case STRAIGHT_BGN:
@@ -77,16 +74,13 @@ int end_compare_main_part(const char* str1, const char* str2, short mode)
             str2--;
             continue;
         }
-
         switch(mode)
         {
             case STRAIGHT_END:
                 return (*str1 - *str2);
-                break;
 
             case REVERSE_END:
                 return (*str2 - *str1);
-                break;
 
             default:
                 break;
@@ -134,16 +128,13 @@ int compare(const char* str1, const char* str2, short mode)
         case STRAIGHT_BGN:
         case REVERSE_BGN:
             return bgn_compare_main_part(str1, str2, mode);
-            break;
 
         case STRAIGHT_END:
         case REVERSE_END:
             return end_compare_main_part(str1, str2, mode);
-            break;
 
         default:
             return UNEXPECTED_MODE;
-            break;
     }
 }
 
@@ -187,14 +178,8 @@ int straight_cmp_from_end       (const void* line1, const void* line2)
     Line* line1_p = (Line*) line1;
     Line* line2_p = (Line*) line2;
 
-//    size_t length_of_str1 = line1_p->length;
-//    size_t length_of_str2 = line2_p->length;
-
     const char* str1 = line1_p->line_ptr + (line1_p->length - 1);
     const char* str2 = line2_p->line_ptr + (line2_p->length - 1);
-
-//    const char* str1 = line1_p->line_ptr + length_of_str1 - 1;
-//    const char* str2 = line2_p->line_ptr + length_of_str2 - 1;
 
     int res = compare(str1, str2, STRAIGHT_END);
     if (res == UNEXPECTED_MODE)
@@ -213,14 +198,8 @@ int reverse_cmp_from_end        (const void* line1, const void* line2)
     Line* line1_p = (Line*) line1;
     Line* line2_p = (Line*) line2;
 
-//    size_t length_of_str1 = line1_p->length;
-//    size_t length_of_str2 = line2_p->length;
-
     const char* str1 = line1_p->line_ptr + (line1_p->length - 1);
     const char* str2 = line2_p->line_ptr + (line2_p->length - 1);
-
-//    const char* str1 = line1_p->line_ptr + length_of_str1 - 1;
-//    const char* str2 = line2_p->line_ptr + length_of_str2 - 1;
 
     int res = compare(str1, str2, REVERSE_END);
     if (res == UNEXPECTED_MODE)
